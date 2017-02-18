ccf5e16a0859885e01a1f60aa20297368839d032
trules <troelsjc@gmail.com>
2017-02-18 23:33:03 +0100

    print example cpr

--- src/Lektion02_BMI/TUI.java
+++ src/Lektion02_BMI/TUI.java
@@ -9,7 +9,11 @@ public class TUI {
     public static void main(String[] args) {
         IFunktionalitet funk = new Funktionalitet();
         Scanner in = new Scanner(System.in);
-        System.out.print("Indtast CPR: ");
+        System.out.println("Indtast CPR: ");
+        System.out.println("f.eks. ");
+        System.out.println("234567-8901");
+        System.out.println("456789-0123");
+        System.out.println("123456-7890");
         String input = in.next();
         System.out.println("Navn: " + funk.getNavn(input) + "\n" + "BMI: " + funk.getBMI(input) + ", " + funk.getTextualBMI(input));
     }

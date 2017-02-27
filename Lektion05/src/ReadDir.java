ef379476b7a61516b3a2dc66b07f5daa1a0227fe
trules <troelsjc@gmail.com>
2017-02-27 19:13:24 +0100

    opg1

--- Lektion05/src/ReadDir.java
+++ Lektion05/src/ReadDir.java
@@ -3,11 +3,12 @@
  */
 import java.io.File;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class ReadDir {
     public static void main(String[] args) {
 
-        File file = null;
+        File filePath = null;
         String[] paths;
         Scanner in;
         in = new Scanner(System.in);
@@ -15,11 +16,11 @@ public class ReadDir {
         inPath = in.nextLine();
 
         try{
-            // create new file object
-            file = new File(inPath);
+            // create new filePath object
+            filePath = new File(inPath);
 
             // array of files and directory
-            paths = file.list();
+            paths = readdir(inPath);
 
             // for each name in the path array
             for(String path:paths)
@@ -28,11 +29,26 @@ public class ReadDir {
                 System.out.println(path);
             }
         }catch(NullPointerException e){
-            System.out.println("Path not found (NullPointerException)");
+            System.out.println("filePath not found (NullPointerException)");
         }
         catch(Exception e){
             // if any error occurs
             e.printStackTrace();
         }
+    }
+
+    private static String[] readdir(String str){
+        File file;
+        String[] path = null;
+        try {
+            // Create new file object
+            file = new File(str);
+
+            // string array of files amd folders
+            path = file.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return path;
     }
 }
